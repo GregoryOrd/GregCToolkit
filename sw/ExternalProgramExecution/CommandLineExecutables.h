beb5f8cdb9beff8eaadadbec703c01b82758b26a
gregoryord <g.ord@hotmail.ca>
2021-02-18 07:33:30 -0700

    Full paths not necessary

--- sw/ExternalProgramExecution/CommandLineExecutables.h
+++ sw/ExternalProgramExecution/CommandLineExecutables.h
@@ -1,10 +1,10 @@
 #ifndef COMMAND_LINE_EXECUTABLES_H
 #define COMMAND_LINE_EXECUTABLES_H
 
-#define mkdir "/usr/bin/mkdir.exe"
-#define gcc "/usr/bin/gcc.exe"
-#define mv "/usr/bin/mv.exe"
-#define cp "/usr/bin/cp.exe"
-#define rm "/usr/bin/rm.exe"
+#define mkdir "mkdir"
+#define gcc "gcc"
+#define mv "mv"
+#define cp "cp"
+#define rm "rm"
 
 #endif
